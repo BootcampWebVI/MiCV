e1abc3db4e36dffb3350f1ae6c22f43d4ec6c208
SabrinaFZ <sabrina.fer.zam@gmail.com>
2018-12-20 21:24:41 +0100

    Fix otros option

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ import { validateForm } from './form.js';
 var menu = document.querySelector('.menu-icon');
 var inputs =  document.querySelectorAll('#form input');
 var textarea = document.querySelector('#form textarea');
-var options = document.querySelectorAll('#form select option')
+var options = document.querySelector('#form select')
 
 menu.addEventListener('click', openMenu.bind(this));
 
@@ -12,9 +12,7 @@ inputs.forEach((input) => {
     input.addEventListener('keyup', validateForm.bind(this));
 });
 
-options.forEach((option) => {
-    option.addEventListener('click', validateForm.bind(this));
-});
+options.addEventListener('click', validateForm.bind(this));
 
 textarea.addEventListener('keyup', validateForm.bind(this));
 
